7a801ba7de823b8e43d87c5da6d0e5c89fb8a255
Steenup <42380499+Steenup@users.noreply.github.com>
2018-08-23 15:11:14 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("button").click(function(){
+   $('h1').html = "elyes rak raw3a"; // .innerHTML
+});
+
+$(".hedhoukom").dblclick(function(){
+   //Code 
+});
+
+$("#hedha").focus(function(){
+   //Code 
+});
